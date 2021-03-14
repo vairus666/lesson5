2ac03bfe4ff1220628a099eb906158014475bccd
vairus666 <43839255+vairus666@users.noreply.github.com>
2021-03-14 23:36:54 +0300

    Create text_lambda.py

---
+++ text_lambda.py
@@ -0,0 +1,6 @@
+#Text lambda
+
+document_text = open('test.txt', 'r')
+text_string = document_text.read().lower()
+count = sum(map(lambda x : 1 if '' in x else 0, text_string)) 
+print(f'Количество вхождений равно = {count}')
