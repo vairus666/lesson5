658f7ce3618087a47c4f460f0f5d9d6957d63aa4
vairus666 <43839255+vairus666@users.noreply.github.com>
2021-03-14 22:50:47 +0300

    Create check_text.py

---
+++ check_text.py
@@ -0,0 +1,28 @@
+# #Check text
+
+import collections
+
+def sum_text(string):
+    return sum(collections.Counter(string).values())     
+
+def sum_words(string): 
+    return string.count(' ') + 1
+
+def sum_sentence(string):
+    if string == '':
+        return 0
+    else:
+        a = string.count('.')
+        b = string.count('?')
+        c = string.count('!')
+        if a | b | c == 0:
+            return 1 
+        return a + b + c
+
+frequency = {}
+document_text = open('test.txt', 'r')
+text_string = document_text.read().lower()
+
+print(f'Количество вхождений равно = {sum_text(text_string)}')
+print(f'Количество слов равно = {sum_words(text_string)}')
+print(f'Количество предложений равно = {sum_sentence(text_string)}')
