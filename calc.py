5bc09084c67e1d2039262baba9d71e6f38c24906
vairus666 <43839255+vairus666@users.noreply.github.com>
2021-03-14 19:59:47 +0300

    Update calc.py

--- calc.py
+++ calc.py
@@ -10,7 +10,7 @@ def calc(a, b, operation):
         return a1 - b1
 
     def multiply(a1, b1):
-            return a1 * b1
+        return a1 * b1
     def divide(a1, b1):
         return a1 / b1
 
@@ -40,16 +40,50 @@ def isfloat(s):
         return False
 
 selector = {"+", "-", "*", "/", "^", "√"}
-x = input('Введите первое значение\n')
-oper = input('Введите операцию (+, -, /, *, ^, √)\n')
-y = input('Введите второе значение\n')
-if isfloat(x):
-    if oper in selector:
-        if isfloat(y):
-                print(f'Результат равен = {calc(float(x),float(y), oper):.3f}')
+x = oper = y = 0
+emp =''
+while x != emp or oper != emp or y != emp:
+    x = input('Введите первое значение\n') 
+    if isfloat(x):
+        oper = input('Введите операцию (+, -, /, *, ^, √)\n')
+        if oper in selector:
+            y = input('Введите второе значение\n')
+            if isfloat(y):
+                try:
+                    print(f'Результат равен = {calc(float(x),float(y), oper):.3f}')
+                    x = input('Введите первое значение\n')
+                    oper = input('Введите операцию (+, -, /, *, ^, √)\n')
+                    y = input('Введите второе значение\n')
+                except Exception as e:
+                    print('Error!!!' + str(e))
+                    print('Введите пример заново')
+                    x = input('Введите первое значение\n')
+                    oper = input('Введите операцию (+, -, /, *, ^, √)\n')
+                    y = input('Введите второе значение\n')
+            elif y == emp:
+                print('вы  не ввели вторую переменную так что пока')
+                break
+            else:
+                print(f'{y} не число')
+                print('Введите пример заново')
+                x = input('Введите первое значение\n')
+                oper = input('Введите операцию (+, -, /, *, ^, √)\n')
+                y = input('Введите второе значение\n')   
+        elif oper == emp:
+            print('вы  не ввели операцию так что пока')
+            break
         else:
-            print(f'{y} не число')   
+            print(f'{oper} - такая операция мне не известна')
+            print('Введите пример заново')
+            x = input('Введите первое значение\n')
+            oper = input('Введите операцию (+, -, /, *, ^, √)\n')
+            y = input('Введите второе значение\n')
+    elif x == emp:
+        print('вы  не ввели первую переменную так что пока')
+        break
     else:
-        print(f'{oper} - такая операция мне не известна')
-else:
-    print(f'{x} не число')
+        print(f'{x} не число')
+        print('Введите пример заново')
+        x = input('Введите первое значение\n')
+        oper = input('Введите операцию (+, -, /, *, ^, √)\n')
+        y = input('Введите второе значение\n')
